b887f5507d30fd2ff8baf73b0a3bdb3ecb54602a
Vrajesh Vagadiya <vrajeshvagadiya06@gmail.com>
2026-04-07 18:43:23 +0200

    Prepare portfolio for deployment

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,11 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import path from 'path'
 
 export default defineConfig({
   plugins: [react()],
-  build: {
-    outDir: path.resolve(__dirname, '../website/static/frontend'),
-    emptyOutDir: true,
-  },
+  base: '/',
 })
