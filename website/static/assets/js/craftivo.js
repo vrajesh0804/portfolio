c7ac8ba0fb1c48eb16c30d1a24a9304a04d2f3d2
Vrajesh Vagadiya <vrajeshvagadiya06@gmail.com>
2026-04-17 00:14:45 +0200

    update changes

--- website/static/assets/js/craftivo.js
+++
@@ -1,6 +0,0 @@
-document.addEventListener('DOMContentLoaded', function(){
-  const nav = document.querySelector('.nav-menu');
-  document.querySelector('.logo').addEventListener('click', () => {
-    nav.classList.toggle('show');
-  });
-});
